bdf8639a7238f1345b81f28357bf6150eea154ad
Michelle Devassia <michelledevassia1999@gmail.com>
2023-06-22 09:45:27 +0530

    version3 sql task1-task8 claysius

--- SQLQuery1 task8-no-error-output1-final CRUD operations with single stored procedure.sql.sql
+++ SQLQuery1 task8-no-error-output1-final CRUD operations with single stored procedure.sql.sql
@@ -7,40 +7,45 @@ Post varchar(50),
 District varchar(50)
 )
 
-EXEC sp_AddressDetails_employees 'INSERT',1,'Michelle',127,'Sasimala','Wayanad'
-EXEC sp_AddressDetails_employees 'INSERT',2,'Kripa',113,'Pulpally','Wayanad'
-EXEC sp_AddressDetails_employees 'INSERT',3,'Bobby',111,'Kappiset','Wayanad'
-EXEC sp_AddressDetails_employees 'UPDATE',2,'Abin',145,'Kattapana','Idukki'
-EXEC sp_AddressDetails_employees 'DELETE',1,'',0,'',''
-EXEC sp_AddressDetails_employees 'SELECT',0,'',0,'',''
+EXEC SP_AddressDetails_employees 'SP_INSERT',1,'Michelle',127,'Sasimala','Wayanad'
+EXEC SP_AddressDetails_employees 'SP_INSERT',2,'Kripa',113,'Pulpally','Wayanad'
+EXEC SP_AddressDetails_employees 'SP_INSERT',3,'Bobby',111,'Kappiset','Wayanad'
+EXEC SP_AddressDetails_employees 'SP_UPDATE',2,'Abin',145,'Kattapana','Idukki'
+EXEC SP_AddressDetails_employees 'SP_DELETE',1,'',0,'',''
+EXEC SP_AddressDetails_employees 'SP_SELECT',0,'',0,'',''
 
-Create Procedure sp_AddressDetails_employees
+Alter Procedure SP_AddressDetails_employees
 (
-@type varchar(50),@Id int ,@Fullname varchar(50),@HouseNo int,@Post varchar(50),@District varchar(50)
+@type AS varchar(50),
+@Id AS int ,
+@Fullname AS varchar(50),
+@HouseNo AS int,
+@Post AS varchar(50),
+@District AS varchar(50)
 )
 As
 Begin
-if @type='INSERT'
+if @type='SP_INSERT'
 	Begin
 		INSERT INTO AddressDetails_employees(Id,Fullname,HouseNo,Post,District)
 		Values (@Id,@Fullname,@HouseNo,@Post,@District)
 
 	End
 
-if @type='UPDATE'
+if @type='SP_UPDATE'
 	Begin
 		UPDATE AddressDetails_employees
 		Set Fullname=@Fullname,HouseNo=@HouseNo,Post=@Post,District=@District
 		where Id=@Id
 	End
 
-	if @type='DELETE'
+	if @type='SP_DELETE'
 	Begin
 		DELETE From AddressDetails_employees
 		where Id=@Id
 	End
 
-	if @type='SELECT'
+	if @type='SP_SELECT'
 	Begin
 		SELECT * From AddressDetails_employees
 		
--- SQLQuery4 task7 CRUD operations with different stored procedure.sql
+++ SQLQuery4 task7 CRUD operations with different stored procedure.sql
@@ -1,4 +1,6 @@
 Use [Employee-details]
+
+========================================
 --stored procedure for inserting data
 Create Procedure sp_dummydatainseert(@Id int,@FirstName varchar(50),@LastName varchar(50),@Age varchar(50))
 As
@@ -6,8 +8,14 @@ BEGIN
 Insert Into Dummydata(FirstName,LastName,Age)
 Values (@FirstName,@LastName,@Age)
 END
-EXEC sp_dummydatainseert 0,'Ganesh','Jose',21
+--to change the name of a procedure,sp_rename is the system stored procedure
+EXEC sp_rename 'sp_dummydatainseert','SPI_insertDummydata'
 
+
+EXEC SPI_insertDummydata @Id=0,@Firstname='Ganesh',@Lastname='Jose',@Age=21
+=================================================
+
+===================================================
 --stored procedure for updating data
 Create Procedure sp_dummydataupdate(@Id int,@FirstName varchar(50),@LastName varchar(50),@Age varchar(50))
 As
@@ -16,8 +24,11 @@ Update Dummydata
 Set FirstName=@FirstName,LastName=@LastName,Age=@Age
 where Id=@Id
 END
-EXEC sp_dummydataupdate 21,'Marisa','Jose',10
+EXEC sp_rename 'sp_dummydataupdate','SPU_updateDummydata'
+EXEC SPU_updateDummydata @Id=28,@FirstName='Marisa',@LastName='Jose',@Age=10
+===============================================
 
+==========================================================
 --stored procedure for delete data
 Create Procedure sp_dummydatadelete(@Id int)
 As
@@ -25,13 +36,21 @@ BEGIN
 Delete Dummydata
 where Id=@Id
 END
-EXEC sp_dummydatadelete 23
+EXEC sp_rename 'sp_dummydatadelete','SPD_deleteDummydata'
+EXEC SPD_deleteDummydata @Id=21
+=================================================
 
+========================================================
 --stored procedure for view all the  data
-Create Procedure sp_dummydataselect
+Alter Procedure SPS_selectDummydata
 As
 BEGIN
-Select * From Dummydata
+Select * From Dummydata Order by Id asc
 END
-EXEC sp_dummydataselect
+EXEC sp_rename 'sp_dummydataselect','SPS_selectDummydata'
 
+EXEC SPS_selectDummydata 
+
+
+======================================================
+===========================================================
